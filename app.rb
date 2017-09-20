927478b8c6e6280e28a2024899e794de952ea905
Student <Student@TC8-MAC19.local>
2017-09-20 15:09:57 -0400

    first project push

---
+++ app.rb
@@ -0,0 +1,16 @@
+#numbers = []
+counter = 0 
+
+100.times do
+	counter = counter + 1
+
+	if counter % 15 == 0
+		puts "fizzbuzz"
+	elsif counter % 3 == 0
+		puts "fizz"
+	elsif counter % 5 == 0
+		puts "buzz"
+	else 
+		puts counter
+	end
+end
